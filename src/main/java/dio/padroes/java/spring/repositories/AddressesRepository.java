6186d83727deb20e7eac5215fceff87cc0b3e963
João Henrique <joaohenriquevidalribeiro@gmail.com>
2023-10-20 14:54:43 -0300

    adjust archives

--- src/main/java/dio/padroes/java/spring/repositories/AddressesRepository.java
+++ src/main/java/dio/padroes/java/spring/repositories/AddressesRepository.java
@@ -1,5 +1,7 @@
 package dio.padroes.java.spring.repositories;
 
+import dio.padroes.java.spring.entities.Address;
+
 
 public class AddressesRepository extends CrudRepository<Address , String>{
     
